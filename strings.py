c1218b9733bcfed7f5c12fce286351b0681fbcc8
peter <393532074@qq.com>
2014-10-15 17:49:55 +0800

    add

---
+++ strings.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+print('the order of A is %d' % ord('A'))
+print('the char of order 98 is %s' % chr(98))
+
+a = 'hello world'
+print('a = ', a)
+print('a[:] = ', a[:])
+print('a[::-1] = ', a[::-1])
+print('a[0:] = ', a[0:])
+print('a[0] = ', a[0])
+print('a[-1] = ', a[-1])
+print('len(a) = ', len(a))
+print('a[1:3] = ', a[1:3])
+print('a[6:-1] = ', a[6:-1])
+print('a[6:] = ', a[6:])
+print('a[6:-1:2] = ', a[6:-1:2]) 
+print('a[:5:-1] = ', a[:5:-1])
