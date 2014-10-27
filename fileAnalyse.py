ba175b60bb2da8e020d7c4bd36707d7a1998c2da
peter <393532074@qq.com>
2014-10-27 15:12:26 +0800

    Changes to be committed: 	modified:   fileAnalyse.py

--- fileAnalyse.py
+++ fileAnalyse.py
@@ -19,10 +19,36 @@ def makeUnique(speech):
 			unique.append(eachword)
 	return unique
 
+def makeUnique_useset(speech):
+	wordSet = set(speech)
+	return wordSet
+
+def makewordDict(speech):
+	wordDict = {}
+	for word in speech:
+		if word in wordDict:
+			wordDict[word] += 1
+		else:
+			wordDict[word] = 1
+	return wordDict
+
+def prettyprint(wordDict):
+	wordLst = []
+	for eachkey in wordDict:
+		wordLst.append((wordDict[eachkey], eachkey))
+	wordLst.sort(reverse = True)
+	print('%-10s%-10s'%('word','count'))
+	print('_'*10)
+	for eachitem in wordLst:
+		print('%-12s%-3d'%(eachitem[1],eachitem[0]))
+
 f = open('Gettysburg.txt', 'rU')
 speech = makeWordList(f)
-unique = makeUnique(speech)
+unique = makeUnique_useset(speech)
+wordDict = makewordDict(speech)
 
 print('the lecture has %d words' % len(speech))
 #print(sorted(unique))
 print('unique length is', len(unique))
+#print('wordDict is', wordDict)
+prettyprint(wordDict)
