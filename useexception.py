9decc8ca2fb1d72ca731adddcf0643b51772f24e
peter <393532074@qq.com>
2014-10-30 16:10:00 +0800

    add useexception.py

---
+++ useexception.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+def processfile(f):
+	if not f.name.endswith('.py'):
+		return ''
+	while True:
+		linestr = f.readline().strip()
+		if linestr.startswith('#'):
+			continue
+		elif len(linestr) == 0:
+			continue
+		elif linestr.startswith("'''") and linestr.endswith("'''"):
+			return linestr.strip("'''")
+		else:
+			return ''
+
+
+def openfile():
+	while True:
+		filename = input('Enter file name:')
+		try:
+			f = open(filename,'r',encoding='utf-8')
+		except IOError:
+			print('Bad filename, try again')
+		else:
+			des = processfile(f)
+			if len(des) > 0:
+				print('the doc desc: ' + des)
+			else:
+				print('no dese')
+		finally:
+			try:
+				f.close()
+			except NameError:
+				pass
+
+openfile()	
+
